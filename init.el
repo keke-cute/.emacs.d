19f7a0e713fa24c113c694f526ba598166a11d88
Keke <keke@RyzenMac.local>
2020-11-01 19:49:00 +0800

    add nasy theme

--- init.el
+++ init.el
@@ -1,3 +1,16 @@
 (require 'org-install)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "readme.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7e94a4eedbc79d5e268d1d364e7a56de469f0702b5418bb02ba5b316283146e4" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
