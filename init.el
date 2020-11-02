47a3694f405aeb3e4b89cb6c8bce4c9c98f13652
Keke <keke@RyzenMac.local>
2020-11-02 15:03:56 +0800

    add xah-fly-key

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7e94a4eedbc79d5e268d1d364e7a56de469f0702b5418bb02ba5b316283146e4" default)))
+   '("08ba1f52f69789cffb7ec15362b38c0ca45cbf595c27d62d415c64007d5a57e8" "7e94a4eedbc79d5e268d1d364e7a56de469f0702b5418bb02ba5b316283146e4" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
