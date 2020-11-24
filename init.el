9afaf7660df2a73c7dd2661e3b1a754360ed1a1b
Shike Liu <liushike1997@gmail.com>
2020-11-24 15:17:26 +0800

    daily update

--- init.el
+++ init.el
@@ -1,16 +1,3 @@
 (require 'org-install)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "readme.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("95d87c512829d31390fdd56a6c5045cd0cd2a35b38baf509c03c85f11c970fe0" "5afcf29b3d73c0959c772321f98735ccb99cca2cf054279202f7568a67828c6c" "13ad38f22ce6b90008d07bae6f0a934e2af8218870617eba1c899def9972b646" "08ba1f52f69789cffb7ec15362b38c0ca45cbf595c27d62d415c64007d5a57e8" "7e94a4eedbc79d5e268d1d364e7a56de469f0702b5418bb02ba5b316283146e4" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
