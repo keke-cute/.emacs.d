7e7dc5aca30b68fe61ca2cf0a341782d25962e5d
Shike Liu <liushike1997@gmail.com>
2020-12-13 20:13:36 +0800

    modified exwm

--- etc/init-guix.el
+++ etc/init-guix.el
@@ -1,8 +1,25 @@
-;;(require 'exwm)
-;;(require 'exwm-config)
-;;(exwm-config-default)
-;;
 (add-to-list 'load-path "/run/current-system/profile/share/emacs/site-lisp/")
+(require 'exwm)
+(require 'exwm-config)
+(exwm-config-example)
+(require 'exwm-systemtray)
+(exwm-systemtray-enable)
+(use-package rime
+  :custom
+  (rime-show-candidate 'posframe)
+  (default-input-method "rime"))
+
+(defun keke/run-in-background (command)
+  (let ((command-parts (split-string command "[ ]+")))
+    (apply #'call-process `(,(car command-parts) nil 0 nil ,@(cdr command-parts)))))
+
+(defun keke/set-wallpaper ()
+  (interactive)
+  (start-process-shell-command
+      "feh" nil  "feh --bg-scale /home/keke/Downloads/86045963_p0.png"))
+
+(keke/set-wallpaper)
+(keke/run-in-background "picom")
 ;;
 ;;(global-set-key (kbd "s-a") 'vterm)
 ;;
@@ -10,7 +27,4 @@
 ;;  `(exwm-input-set-key ,(kbd key) (lambda () (interactive) ,command)))
 ;;(defmacro keke/exwm-bind-exec (key cmd)
 ;;  `(keke/exwm-bind-lambda ,key (start-process-shell-command ,cmd nil ,cmd)))
-;;(keke/exwm-bind-exec "s-f" "firefox")
-;;
-;;
-;;(require 'vterm)
+;;(keke/exwm-bind-exec "s-f" "firefox"
